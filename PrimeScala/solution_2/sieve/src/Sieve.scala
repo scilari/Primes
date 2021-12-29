68079258a5eea1a48c2a74b3b27307eb66918604
Ilari Vallivaara <ilari.vallivaara@gmail.com>
2021-12-29 02:28:40 +0000

    Add parallel comparison code

--- PrimeScala/solution_2/sieve/src/Sieve.scala
+++ PrimeScala/solution_2/sieve/src/Sieve.scala
@@ -1,5 +1,9 @@
 import Math.sqrt
 import scala.annotation.tailrec
+import scala.collection.parallel.ForkJoinTaskSupport
+import scala.util.Success
+import scala.util.Failure
+import java.util.concurrent.Executors
 
 final class Sieve(val size: Int) {
   // Allocate one extra element for sentinel value
@@ -24,12 +28,12 @@ final class Sieve(val size: Int) {
   private[this] def notPrime(k: Int): Boolean = bits(k >> 1)
 
   @tailrec
-  private[this] def nextPrime(i: Int): Int = {
+  def nextPrime(i: Int): Int = {
     if (notPrime(i)) nextPrime(i + 2) else i
   }
 
   @tailrec
-  private[this] def markMultiples(i: Int, increment: Int): Unit = {
+  def markMultiples(i: Int, increment: Int): Unit = {
     if (i < size) {
       markBit(i)
       markMultiples(i + increment, increment)
@@ -50,19 +54,63 @@ object Sieve {
   val sieveSize = 1000000
   val runTimeMs: Long = 5000
   
-  val warmUp = false
+  val warmUp = true
   val warmUpTimeMs: Long = 5000
 
-  val validate = false
+  val validate = true
 
   def main(args: Array[String]): Unit = {
     if (warmUp) measurePasses(sieveSize, warmUpTimeMs)
 
-    val (passes, time) = measurePasses(sieveSize, runTimeMs)
-
-    println(s"scilari;$passes;$time;1;algorithm=base,faithful=yes")
+    parallelVsSerial
 
     if (validate) validateResults
+  }
+
+  def parallelVsSerial = {
+    import scala.collection.parallel.CollectionConverters._
+    import scala.concurrent.{Await, Future}
+    import scala.concurrent.duration._
+    import scala.concurrent.ExecutionContext.Implicits.global
+
+    println("Running parallel vs serial comparison")
+    def largeTask() = {
+      (0 until 5000).map{_ => new Sieve(1000000).primeCount}
+    }
+
+    // 100X smaller
+    def smallTask() = {
+      (0 until 5000).map{_ => new Sieve(10000).primeCount} 
+    }
+
+    def selectTask(i: Int, total_t0: Long) =  {
+      val t0 = System.currentTimeMillis
+      val k = if (i % 3 == 0) smallTask() else largeTask()
+      val t = System.currentTimeMillis
+      println(s"Task took: ${t - t0} (total: ${t - total_t0})")
+      k
+    }
+
+    val runCount = 20
+    println("\nSerial run:")
+    val t0Serial = System.currentTimeMillis
+    val foundCountsSer = (0 until runCount).map { i => 
+      selectTask(i, t0Serial)
+    }
+    println(s"Running tasks serially took ${System.currentTimeMillis - t0Serial} ms")
+
+    val t0Parallel = System.currentTimeMillis
+
+    
+    println(s"\nParallel run:")
+
+    (0 until runCount).par.foreach{ i =>
+      selectTask(i, t0Parallel)
+    }
+
+    println(s"Running tasks parallel took ${System.currentTimeMillis - t0Parallel} ms")
+
+  
   }
 
   def measurePasses(sieveSize: Int, runTimeMs: Long): (Int, Double) = {
@@ -105,5 +153,7 @@ object Sieve {
       )
     }
   }
+
+
 
 }
